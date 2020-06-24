48836bbffcce68731fe17575a6520cf032ea08a8
Michal Medvecky <miskomedvecky@gmail.com>
2020-06-24 18:41:31 +0200

    Added difference list implementation.

--- src/Main.hs
+++ src/Main.hs
@@ -21,14 +21,10 @@ insertCol :: [[Char]]   -- ^ Input (2D array).
             -> Int      -- ^ Column, to which the word sohuld be inserted.
             -> Int      -- ^ Position in column, at which the word should be inserted.
             -> [[Char]] -- ^ Output.
-insertCol i w col p = insertCol' i w col 0 p
-
--- | Helper function for 'insertCol', with one before the last argument being row counter (counting from 0).
-insertCol' :: [[Char]] -> [Char] -> Int -> Int -> Int -> [[Char]]
-insertCol' i [] _ _ _ = i
-insertCol' (i:is) (w:ws) col c p
-    | p == c    = (replaceItem i [w] col) : insertCol' is ws col c p
-    | otherwise = i : (insertCol' is (w:ws) col (c + 1) p)
+insertCol i [] _ _ = i
+insertCol (i:is) (w:ws) col p
+    | p == 0    = (replaceItem i [w] col) : insertCol is ws col p
+    | otherwise = i : (insertCol is (w:ws) col (p - 1))
 
 -- | Function 'insertRow' will get 2D array of chars and will write down provided word into selected row at given position.
 insertRow :: [[Char]]   -- ^ Input (2D array).
@@ -36,28 +32,19 @@ insertRow :: [[Char]]   -- ^ Input (2D array).
             -> Int      -- ^ Row, to which the word should be inserted.
             -> Int      -- ^ Position, at which the word should be inserted.
             -> [[Char]] -- ^ Output.
-insertRow i w r p = insertRow' i w r 0 p
-
--- | Helper function for 'insertRow', with one before the last argument being row counter (counting from 0).
-insertRow' :: [[Char]] -> [Char] -> Int -> Int -> Int -> [[Char]]
-insertRow' (i:is) w r c p
-    | r == c    = (replaceItem i w p) : is
-    | otherwise = i : insertRow' is w r (c + 1) p
+insertRow (i:is) w r p
+    | r == 0    = (replaceItem i w p) : is
+    | otherwise = i : insertRow is w (r - 1) p
 
 -- | Function will replace part of given array at given position.
 replaceItem :: (Ord a) => [a] -- ^ Input array.
                 -> [a]        -- ^ Replacement array.
                 -> Int        -- ^ Where to replace.
                 -> [a]        -- ^ Output.
-replaceItem a i b
-    | b >= (length a) = a
-    | otherwise       = replaceItem' a i b 0
-
-replaceItem' :: (Ord a) => [a] -> [a] -> Int -> Int -> [a]
-replaceItem' a [] _ _ = a
-replaceItem' (a:as) (i:is) b c
-    | b == c    = i : (replaceItem' as is b c)
-    | otherwise = a : (replaceItem' as (i:is) b (c + 1))
+replaceItem a [] _ = a
+replaceItem (a:as) (i:is) b
+    | b == 0    = i : (replaceItem as is b)
+    | otherwise = a : replaceItem as (i:is) (b - 1)
 
 data RowSpace = WordSpace (Int, Int) (Int, Int) Bool
 
@@ -66,3 +53,38 @@ data RowSpace = WordSpace (Int, Int) (Int, Int) Bool
 -- Posivtive values means rows, negative values means columns.
 spaceLen :: RowSpace -> Int
 spaceLen (WordSpace (a1, a2) (b1, b2) _) =  (b1 - a1) + (-1 * (b2 - a2))
+
+-- | Difference list implementation.
+--
+-- Difference list have both append and head operations with O(1) complexity and therefore are perfect replacement for queue.
+newtype DiffList a = DiffList {getDiffList :: [a] -> [a]}
+
+instance (Show a) => Show (DiffList a) where
+    show (DiffList f) = show (f [])
+
+instance Semigroup (DiffList a) where
+    DiffList a <> DiffList b = DiffList (\dl -> a . b $ dl)
+
+instance Monoid (DiffList a) where
+    mempty = DiffList (\dl -> [] ++ dl)
+
+toDiffList :: [a] -> DiffList a
+toDiffList a = DiffList (a ++)
+
+dlToList :: DiffList a -> [a] -> [a]
+dlToList (DiffList a) b = a b
+
+dlAppend :: a -> DiffList a -> DiffList a
+dlAppend a dl = dl <> (DiffList ([a] ++))
+
+dlHead :: DiffList a -> a
+dlHead dl = head (dlToList dl [])
+
+dlPop :: (Eq a) => DiffList a -> Maybe (DiffList a)
+dlPop dl
+    | dl == mempty = Nothing
+    | otherwise    = Just (toDiffList (tail (dlToList dl []))) 
+
+instance (Eq a) => Eq (DiffList a) where 
+    a == b = (dlToList a []) == (dlToList b [])
+    a /= b = (dlToList a []) /= (dlToList b [])
